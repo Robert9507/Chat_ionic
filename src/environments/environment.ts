207f7ed92203f42da3e9a8d034fc69321a7e0fa9
Roberto Chacón <75052626+Robert9507@users.noreply.github.com>
2023-12-04 21:59:24 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,7 @@
 
 export const environment = {
   production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyBcSdRd6GC556O_miOlYCUYsiRWB0X3DJ4",
-    authDomain: "fir-rn-69c21.firebaseapp.com",
-    projectId: "fir-rn-69c21",
-    storageBucket: "fir-rn-69c21.appspot.com",
-    messagingSenderId: "204073559238",
-    appId: "1:204073559238:web:4c1b2430b9faf3c2cc23c7"
-  }
+
 };
 
 /*
